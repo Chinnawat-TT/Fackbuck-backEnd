2a3f398526f56fa2c96b008cdc0ece08d20465bc
Chinnawat-TT <Chinnawat.sangrung@gmail.com>
2023-10-06 17:14:58 +0700

    add feature post

--- src/controllers/post-controller.js
+++ src/controllers/post-controller.js
@@ -2,6 +2,18 @@ const fs = require('fs/promises')
 const prisma = require("../models/prisma")
 const { upload } = require("../utils/cloudinary-service")
 const createError = require("../utils/create-error")
+const { STATUS_ACCEPTED } = require('../config/constans')
+
+const getFriendId = async(tagetUserId)=>{
+    const relationship = await prisma.friend.findMany({
+        where : {
+            OR :[{receiverId : tagetUserId},{requesterId : tagetUserId}],
+            status : STATUS_ACCEPTED
+        }
+    })
+    const friendIds = relationship.map( el => el.requesterId === tagetUserId ? el.receiverId : el.requesterId)
+    return friendIds
+}
 
 exports.createPost = async (req , res ,next)=>{
 try {
@@ -28,4 +40,35 @@ try {
         fs.unlink(req.file.path)
     }
 }
+}
+
+exports.getAllPostIncludeFriendPost = async (req,res,next) =>{
+    try {
+        const friendIds = await getFriendId(req.user.id)
+        const posts = await prisma.post.findMany({
+            where :{
+                userId :{
+                    in :[...friendIds, req.user.id]
+                }
+            },orderBy :{
+                createdAt : 'desc'
+            },include :{
+                user :{
+                select :{
+                    id : true,
+                    firstName:true,
+                    lastName:true,
+                    profileImage:true
+                }
+                },likes : {
+                    select : {
+                        userId : true
+                    }
+                }
+            }
+        })
+        res.status(200).json({ posts })
+    } catch (err) {
+        next(err)
+    }
 }
--- src/routes/post-route.js
+++ src/routes/post-route.js
@@ -3,9 +3,12 @@ const express = require('express')
 const authenticateMiddleware = require('../middleware/authenticate')
 const uploadMiddleware = require('../middleware/upload')
 const postController = require('../controllers/post-controller')
+const likeController = require('../controllers/like-controller')
 
 const router = express.Router()
 
 router.post('/',authenticateMiddleware ,uploadMiddleware.single('image'),postController.createPost)
+router.get('/friend',authenticateMiddleware,postController.getAllPostIncludeFriendPost)
+router.post('/:postId/like',authenticateMiddleware,likeController.toggleLike)
 
 module.exports = router
